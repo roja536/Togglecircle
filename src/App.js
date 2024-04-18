0ea8f356584a788ac599f214c29f2b4e3afc5045
roja <roja.jakkala@gmail.com>
2024-04-18 16:34:58 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 
 import './App.css';
-import CustomHook1 from './Components/CustomHook1';
-//import DeleteStudent from './Components/DeleteStudent';
-//import Gallery from './Components/Gallery';
-// import ContactUs from './Components/ContactUs';
-
-// import Header from './Components/Header';
-
-// import Home from './Components/Home';
-// import AboutUs from './Components/AboutUs';
-// import Courses from './Components/Courses';
-// import Gallery from './Components/Gallery';
-// import ContactUs from './Components/ContactUs'
-// import Footer from './Components/Footer'
-
-// import { Routes, Route } from 'react-router-dom';
-//import BasicRead from './Components/BasicRead';
-//import Hooks from './Components/Hooks';
-
-import Form from '../src/Contact_Management/Form'
+import Todo from './Components/Todo';
 
 function App() {
   return (
     <>
-      {/* <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/aboutus' element={<AboutUs />} />
-        <Route path='/courses' element={<Courses />} />
-        <Route path='/gallery' element={<Gallery />} />
-        <Route path='/contactus' element={<ContactUs />} />
-      </Routes>
-      <Footer /> */}
-      {/* <BasicRead /> */}
-      {/* <Hooks /> */}
-      {/* <AddStudent /> */}
-      {/* <DeleteStudent /> */}
-      {/* <CustomHook1 /> */}
-      <Form />
-
+      <Todo />
     </>
 
   );
